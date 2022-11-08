6fec523694a1ec7d0f0f215c5bcbcff3c70e4010
tanzin-rahman <45721478+tanzin-rahman@users.noreply.github.com>
2022-11-08 12:34:06 -0800

    Simplified!

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -107,16 +107,10 @@ const months = [
   'December'
 ];
 
+let sDate = dueDate;
 // return 'YYYY-MM-DD'
-function getShortDate(sDate){
-  let firstDateFormat = sDate.getFullYear()+'-'+sDate.getMonth()+1+'-'+sDate.getDate();
-  console.log(firstDateFormat);
-  return firstDateFormat;
-}
-function getLongDate(lDate){
-  let secondDateFormat = months[lDate.getMonth()]+' '+lDate.getDate()+', '+lDate.getFullYear();
-  console.log(secondDateFormat);
-  return secondDateFormat;
-}
+const formattedDate = dueDate.getFullYear()+'-'+dueDate.getMonth()+1+'-'+dueDate.getDate();
+const stringDate = months[dueDate.getMonth()]+' '+dueDate.getDate()+', '+dueDate.getFullYear();
+
 // 10. log this value using console.log
-console.log("<time datetime=\""+getShortDate(dueDate)+"\">"+getLongDate(dueDate)+"</time>");
+console.log(`<time datetime="${formattedDate}"> ${stringDate} </time>`);
